d7dc0df459108537e1425310d2aff9b67333ce8c
tofid <andreyklochok@gmail.com>
2016-08-04 14:05:47 +0000

    Initial commit

---
+++ hipanel-notify-bar.js
@@ -0,0 +1,8 @@
+var s, HipanelNotifyBar = {
+    options: {
+       headerText: 'Would you like to try new panel?',
+       yesText: 'Yes',
+       noText: 'No',
+       newPanelLink: 'http://hipanel.local'
+    }
+};
